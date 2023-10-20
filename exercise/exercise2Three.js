495012353a32021a88fac272e0ef8d6467a28b77
kareraolivier <kareraolivier@gmail.com>
2023-10-20 11:05:25 +0200

    third exercise

--- exercise/exercise2Three.js
+++ exercise/exercise2Three.js
@@ -1,1 +1,10 @@
 // - https://www.codewars.com/kata/585db3e8eec141ce9a00008f
+
+const reverseVowels = (str) => {
+  const checkVowels = (str) =>
+    ["i", "u", "o", "a", "e"].includes(str.toLowerCase());
+  const vowels = [...str].filter((str) => checkVowels(str));
+  return [...str]
+    .map((str) => (checkVowels(str) ? vowels.pop() : str))
+    .join("");
+};
